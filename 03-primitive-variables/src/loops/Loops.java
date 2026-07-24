677b9cb97741c1850aed4ea79af707cb84fdc3d6
jessus-thread <jessus.thread@outlook.com>
2026-07-24 12:59:46 -0600

    feat: The While and Do While loop class ended

--- 03-primitive-variables/src/loops/Loops.java
+++ 03-primitive-variables/src/loops/Loops.java
@@ -3,11 +3,43 @@ package loops;
 public class Loops {
     public static void main(String[] args) {
         Loops.exampleLoopFor();
+        Loops.exampleLoopWhile();
+        Loops.exampleLoopDoWhile();
+    }
+
+    /*
+        It is executed at least once and then evaluates the request
+    */
+    private static void exampleLoopDoWhile() {
+        int counter = 0;
+
+        do {
+            System.out.println("Coounter Do While: " + counter);
+
+            counter++;
+        } while (counter < 5);
+    }
+
+    private static void exampleLoopWhile() {
+        int counter = 0;
+        int accumulator = 0;
+
+        while (counter < 5) {
+            System.out.println("Counter While: " + counter);
+
+            accumulator = accumulator + counter;
+            counter++;
+        }
+
+        System.out.println("Accumulator: " + accumulator);
     }
 
     private static void exampleLoopFor() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Counte: " + i);
+        // Literal constant
+        final int MAX_COUNT = 5;
+
+        for (int currentNumber = 0; currentNumber <= MAX_COUNT; currentNumber++) {
+            System.out.println("Counte: " + currentNumber);
         }
     }
 }
