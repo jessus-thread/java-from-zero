602d03e020946ff0d35605eb1a31afd37afc9eb1
jessus-thread <jessus.thread@outlook.com>
2026-07-24 12:41:08 -0600

    feat: The foor loop class was terminated

---
+++ 03-primitive-variables/src/loops/Loops.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class Loops {
+    public static void main(String[] args) {
+        Loops.exampleLoopFor();
+    }
+
+    private static void exampleLoopFor() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Counte: " + i);
+        }
+    }
+}
