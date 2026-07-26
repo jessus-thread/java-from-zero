b1b73935eea658e5b810d0a10e418b7948848088
jessus.thread <jessus.thread@outlook.com>
2026-07-25 20:51:38 -0600

    feat: the access modifiers clas has been completed

--- classes-objects/src/HouseExample/House.java
+++ classes-objects/src/HouseExample/House.java
@@ -1,9 +1,25 @@
 package HouseExample;
 
 public class House {
-    int doors;
-    int windows;
-    double size;
+    /*
+        Access modifiers help us encapsulate information and protect our data within a class.
+
+        Without a doubt, access modifiers are related to a key programming concept called
+        encapsulation.
+
+        Mastering this concept is key to creating robust and secure software because it
+        allows us to control access to the attributes and methods of our class.
+
+        It is important to know that when we do not use an access modifier on a
+        property/method, by default it is protected.
+
+        The protected attribute is used to protect properties/methods and allows
+        them to be accessed by other classes within the same package.
+     */
+
+    private int doors;
+    private int windows;
+    private double size;
 
     /*
         A constructor is a special method used to initialize an object of a class; that is,
@@ -36,11 +52,50 @@ public class House {
         this.size = size;
     }
 
-    void openDoor() {
+    public void openDoor() {
         System.out.println("Open door");
     }
 
-    double calculateArea() {
+    public double calculateArea() {
         return size * size;
+    }
+
+    /*
+        Getters have direct access to the properties of a class and are public,
+        so they can be accessed from outside the class.
+
+        This allows us to control how properties are accessed; we can even perform
+        operations before returning them.
+     */
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getWindows() {
+        return windows;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    /*
+        Setters have direct access to the properties of a class and are public,
+        so they can be accessed from outside the class.
+
+        This allows us to control how properties are modified; we can even perform
+        operations before modifying them.
+     */
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setWindows(int windows) {
+        this.windows = windows;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
     }
 }
--- classes-objects/src/HouseExample/HouseMain.java
+++ classes-objects/src/HouseExample/HouseMain.java
@@ -5,9 +5,18 @@ public class HouseMain {
         House firstHouse = new House();
         House secondHouse = new House(3, 4, 20);
 
-        firstHouse.doors = 3;
-        firstHouse.windows = 5;
-        firstHouse.size = 10;
+        /*
+            Permitir acceder a los atributos de una clase de forma
+            directa es una mala práctica ya que viola el principio de encapsulamiento.
+
+            firstHouse.doors = 3;
+            firstHouse.windows = 5;
+            firstHouse.size = 10;
+        */
+
+        firstHouse.setDoors(3);
+        firstHouse.setWindows(5);
+        firstHouse.setSize(10);
 
         firstHouse.openDoor();
         secondHouse.openDoor();
