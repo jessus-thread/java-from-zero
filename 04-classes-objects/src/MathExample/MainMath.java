666c195963c2938d004f8b1ea2315979b7b8836b
jessus.thread <jessus.thread@outlook.com>
2026-07-26 20:47:11 -0600

    feat: the class methods - static class has ended

---
+++ 04-classes-objects/src/MathExample/MainMath.java
@@ -0,0 +1,11 @@
+package MathExample;
+
+public class MainMath {
+    public static void main(String[] args) {
+        int result = MathExample.add(10, 10);
+        int subtractResult = MathExample.subtract(20, 20);
+
+        System.out.println("The sum is: " + result);
+        System.out.println("The subtract result is: " + subtractResult);
+    }
+}
---
+++ 04-classes-objects/src/MathExample/MathExample.java
@@ -0,0 +1,11 @@
+package MathExample;
+
+public class MathExample {
+    public static int add(int numberOne, int numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+    public static int subtract(int numberOne, int numberTwo) {
+        return numberOne - numberTwo;
+    }
+}
